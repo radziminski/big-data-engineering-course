b4374869467a53f601dd1dad50dfa3101603bc7b
Jan Radziminski <radziminski.j@gmail.com>
2020-12-06 19:33:38 +0100

    Added reading from file

--- lab3/KafkaProducerConsumer/src/main/java/at/ac/fhsalzburg/bde/app/SpotifyProducer.java
+++ lab3/KafkaProducerConsumer/src/main/java/at/ac/fhsalzburg/bde/app/SpotifyProducer.java
@@ -3,15 +3,19 @@ package at.ac.fhsalzburg.bde.app;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.UUID;
 
 public class SpotifyProducer implements Runnable {
-    public final static String TOPIC = "groupB04.spotifytest";
+    public final static String TOPIC = "groupB04.spotifytempos";
     private final static String CLIENT_ID = SpotifyProducer.class.getName();
 
-    private static final int DELAY_BASE = 1000;
+    private static final int DELAY_BASE = 500;
 
     private final Producer<Long, String> producer;
 
@@ -20,8 +24,8 @@ public class SpotifyProducer implements Runnable {
         this.producer = producer;
     }
 
-    private String getJsonValue(int id, int tempo, int year) {
-        return String.format("[%d,%d,%d]",
+    private String getJsonValue(String id, String year, String tempo) {
+        return String.format("[%s,%s,%s]",
             id,
             year,
             tempo);
@@ -30,34 +34,68 @@ public class SpotifyProducer implements Runnable {
     @Override
     public void run() {
         System.out.println("=== SONG PRODUCER ===");
+        try {
+            FileReader fr = new FileReader("spotify.csv");
+            BufferedReader br = new BufferedReader(fr);
 
-        ProducerRecord<Long, String> record;
-        long key;
-        String value;
 
-        for (int i = 0; i < 30; i++) {
-            key = i % 10;
-            value = getJsonValue(i, 128 + i, 1921 + i*10);
-            record = new ProducerRecord<>(TOPIC, key, value);
-            producer.send(record);
+            int rowLength = 19;
+            String line;
+            ProducerRecord<Long, String> record;
+            long key;
+            String value;
+            int skipLines = 1;
 
-            System.out.printf("sent record topic = %s, key = %d, value = %s\n",
-                    TOPIC,
-                    key,
-                    value);
+            while ((line = br.readLine()) != null) {
+                if (skipLines-- > 0) continue;
 
-            try {
-                Thread.sleep(DELAY_BASE);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                // just to get different years
+                for (int i = 0; i < 100; i++) br.readLine();
+
+                String[] parts = line.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                if (parts.length != rowLength) continue;
+                String tempo = parts[18];
+                String year = parts[1];
+                String id = parts[8];
+
+                try {
+                    key = Long.parseLong(year);
+                } catch(NumberFormatException error) {
+                    System.out.println("Wrong year - cant convert to long\n");
+                    continue;
+                }
+
+                value = getJsonValue(id, year, tempo);
+
+                record = new ProducerRecord<>(TOPIC, key, value);
+                producer.send(record);
+
+                System.out.printf("[sent to %s topic] MsgKey = %d, Value = %s\n",
+                        TOPIC,
+                        key,
+                        value);
+                
+                try {
+                    Thread.sleep(DELAY_BASE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
+
+            System.out.println("closing ...");
+            producer.flush();
+            producer.close();
+
+            System.out.println("done.");
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+            return;
+        } catch(IOException e) {
+            System.out.println("IO exception");
+            return;
         }
-
-        System.out.println("closing ...");
-        producer.flush();
-        producer.close();
-
-        System.out.println("done.");
+        
     }
 
     public static void main(String[] args) {
